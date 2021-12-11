b66332714e3623a79795b5a968f10c73f0a34d3d
Adam Norris <anorris@rivasolutionsinc.com>
2021-12-11 14:02:06 -0500

    Updated non-spinnaker eks configuration

---
+++ ingress.tf
@@ -0,0 +1,10 @@
+data "kubernetes_service" "ingress_nginx" {
+  metadata {
+    name      = "ingress-nginx-controller"
+    namespace = "ingress-nginx"
+  }
+
+  depends_on = [
+    helm_release.ingress-controller,
+  ]
+}
