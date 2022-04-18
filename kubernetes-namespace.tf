61dbf309028929f5f4fadff02cc82a53dbcdcfc3
Adam Norris <anorris@rivasolutionsinc.com>
2022-04-17 20:56:20 -0400

    Removed extra namespaces

--- kubernetes-namespace.tf
+++ kubernetes-namespace.tf
@@ -10,24 +10,3 @@ resource "kubernetes_namespace" "extra" {
     module.eks,
   ]
 }
-
-# resource "kubernetes_namespace" "cert-manager" {
-#   metadata {
-#     name = "cert-manager"
-#   }
-
-#   depends_on = [
-#     module.eks,
-#   ]
-# }
-
-# resource "kubernetes_namespace" "ingress-nginx" {
-#   metadata {
-#     name = "ingress-nginx"
-#   }
-
-#   depends_on = [
-#     module.eks,
-#   ]
-# }
-
